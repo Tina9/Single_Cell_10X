4c134b1bb43de0185e7e18cbdc4689c145844209
xu zhang <xuhzhang@outlook.com>
2020-10-13 10:45:36 +0800

    Delete repeated analysis

--- Seurat.R
+++ Seurat.R
@@ -155,38 +155,6 @@ data_list <- list(control_data, exp_data)
 names(data_list) <- c("control", "exp")
 data_list <- lapply(X = data_list, FUN = prepare_data, min_feature = 200,
                     max_feature = 7000, mt_percent = 15)
-aggr1_2_3_combined_data <- data_integration(data_list)
-seeking_clusters(aggr1_2_3_combined_data)
-
-### Get cell cluster
-metadata <- aggr1_2_3_combined_data@meta.data %>% as.data.frame
-### Get Expression Values
-ExpressData <- GetAssayData(aggr1_2_3_combined_data[["integrated"]], slot = "data")
-
-### Annotate Cell Clusters
-MetaData <- ClusterFinding(ExpressData, metadata)
-
-### Keep data for next usuage ##############
-save(aggr1_2_3_combined_data, file = "HP4313_combined_data.RData")
-write.table(MetaData, file = "HP4313_combined_metadata.txt", sep = "\t", quote = F)
-write.table(as.data.frame(ExpressData), file = "HP4313_features.csv", sep = ",", quote = F)
-
-#############################################################
-############ Analysis for HP4313 ############################
-#############################################################
-setwd("~/Dropbox/BRCA1-PARPi-10X/Latest Version/cluster_finding/HP4313/")
-control_data_dir <- "~/Dropbox/BRCA1-PARPi-10X/Seurat_Raw_Data/HP4313Pr/"
-control_name <- "control"
-exp_data_dir <- "~/Dropbox/BRCA1-PARPi-10X/Seurat_Raw_Data/HP4313Re/"
-exp_name <- "exp"
-control_data <- read_data(control_data_dir, control_name)
-exp_data <- read_data(exp_data_dir, exp_name)
-plot_data <- rbind(control_data@meta.data, exp_data@meta.data)
-density_plot(plot_data)
-data_list <- list(control_data, exp_data)
-names(data_list) <- c("control", "exp")
-data_list <- lapply(X = data_list, FUN = prepare_data, min_feature = 200,
-                    max_feature = 7000, mt_percent = 15)
 combined_data <- data_integration(data_list)
 seeking_clusters(combined_data)
 
